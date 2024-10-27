a0fa2e74b0d45c261148161a9fefeced9768d338
SanthoshArigela <atsssanthosh@gmail.com>
2024-10-27 18:03:52 +0530

    Uploading code: Surface Area and Volume of a Cube.c

---
+++ Surface Area and Volume of a Cube.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,s,v;
+    scanf("%d",&a);
+    s=6*a*a;
+    v=(a*a)*a;
+    printf("Surface area = %d and Volume = %d",s,v);
+    return 0;
+}
