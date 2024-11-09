aee489cdb831b9f8c9183792b55f1001a2f334d8
SanthoshArigela <atsssanthosh@gmail.com>
2024-11-09 23:48:03 +0530

    Uploading code: Subscription.c

---
+++ Subscription.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>40)
+    {
+        printf("YES",a);
+    }
+    else
+    {
+        printf("NO",a);
+    }
+    return 0;
+}
