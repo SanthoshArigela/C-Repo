5d149481b7af8faf0752da86e47ece2155c76983
SanthoshArigela <atsssanthosh@gmail.com>
2024-11-12 22:32:14 +0530

    Uploading code: Parliament.c

---
+++ Parliament.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if((b*2)>=a)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    return 0;
+}
